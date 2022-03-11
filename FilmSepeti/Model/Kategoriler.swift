5a2859c15c10f88ada8e9b4803a3fafd29a3da8b
sefabicerr <sefaabicerr@gmail.com>
2022-03-11 19:20:56 +0300

    no message

---
+++ FilmSepeti/Model/Kategoriler.swift
@@ -0,0 +1,27 @@
+//
+//  Categories.swift
+//  FilmSepeti
+//
+//  Created by Muhammed Sefa Biçer on 17.02.2022.
+//
+
+import Foundation
+
+
+class Kategoriler:Codable{
+    
+    var kategori_id: String?
+    var kategori_ad: String?
+    
+    init(){
+    }
+    
+    init(kategori_id:String, kategori_ad:String){
+        
+        self.kategori_id = kategori_id
+        self.kategori_ad = kategori_ad
+    }
+    
+}
+
+
